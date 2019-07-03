18db13b08fff9f706f77048e4210ea8e12142522
Neil T <neiltz711@gmail.com>
2019-07-03 12:34:27 -0400

    Update notif

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -119,7 +119,46 @@ exports.addUserDetails = (req, res) => {
     });
 };
 
-// Get the user details
+// Get any user's details
+exports.getUserDetails = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.params.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        userData.user = doc.data();
+        // firebase index created
+        return db
+          .collection('thoughts')
+          .where('userHandle', '==', req.params.handle)
+          .orderBy('createdAt', 'desc')
+          .get();
+      } else {
+        return res.status(404).json({ error: 'User not found' });
+      }
+    })
+    .then(data => {
+      userData.thoughts = [];
+      data.forEach(doc => {
+        userData.thoughts.push({
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+          userHandle: doc.data().userHandle,
+          likeCount: doc.data().likeCount,
+          commentCount: doc.data().commentCount,
+          userImage: doc.data().userImage,
+          thoughtId: doc.id
+        });
+      });
+      return res.json(userData);
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+// Get own user's details
 exports.getAuthenticatedUser = (req, res) => {
   let userData = {}; // response data
 
@@ -137,6 +176,26 @@ exports.getAuthenticatedUser = (req, res) => {
       userData.likes = [];
       data.forEach(doc => {
         userData.likes.push(doc.data());
+      });
+      // index created
+      return db
+        .collection('notifications')
+        .where('recipient', '==', req.user.handle)
+        .orderBy('createdAt', 'desc')
+        .limit(10)
+        .get();
+    })
+    .then(data => {
+      userData.notifications = [];
+      data.forEach(doc => {
+        userData.notifications.push({
+          recipient: doc.data().recipient,
+          sender: doc.data().sender,
+          createdAt: doc.data().createdAt,
+          thoughtId: doc.data().thoughtId,
+          read: doc.data().read,
+          notificationId: doc.data().notificationId
+        });
       });
       return res.json(userData);
     })
@@ -205,4 +264,22 @@ exports.uploadImage = (req, res) => {
       });
   });
   busboy.end(req.rawBody);
+};
+
+exports.markNotificationsRead = (req, res) => {
+  // batch write - update multiple docs
+  let batch = db.batch();
+  req.body.forEach(notificationId => {
+    const notification = db.doc(`/notifications/${notificationId}`);
+    batch.update(notification, { read: true });
+  });
+  batch
+    .commit()
+    .then(() => {
+      return res.json({ message: 'Notifications marked read' });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
 };
--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,9 @@ const functions = require('firebase-functions');
 //const express = require('express');
 //const app = express();
 const app = require('express')();
-
 const FBAuth = require('./util/fbAuth');
+
+const { db } = require('./util/admin');
 
 const {
   getAllThoughts,
@@ -19,7 +20,9 @@ const {
   login,
   uploadImage,
   addUserDetails,
-  getAuthenticatedUser
+  getAuthenticatedUser,
+  getUserDetails,
+  markNotificationsRead
 } = require('./handlers/users');
 
 // Thought route
@@ -37,6 +40,76 @@ app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails); // public route
+app.post('/notifications', FBAuth, markNotificationsRead);
 
 // https://baseurl.com/api/
 exports.api = functions.https.onRequest(app);
+
+// notif when thoughts are made - database triggers
+exports.createNotificationOnLike = functions.firestore
+  .document('likes/{id}')
+  .onCreate(snapshot => {
+    return db
+      .doc(`/thoughts/${snapshot.data().thoughtId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            thoughtId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnlike = functions.firestore
+  .document('likes/{id}')
+  .onDelete(snapshot => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions.firestore
+  .document('comments/{id}')
+  .onCreate(snapshot => {
+    db.doc(`/thoughts/${snapshot.data().thoughtId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            thoughtId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
